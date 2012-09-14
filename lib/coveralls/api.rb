f6a6ac637e89e3bd22eb59c6476ce9e4e87f70f7
Wil Gieseler <supapuerco@gmail.com>
2012-09-14 13:18:38 -0700

    report travis job id

--- lib/coveralls/api.rb
+++ lib/coveralls/api.rb
@@ -21,9 +21,14 @@ module Coveralls
 		end
 
 		def self.hash_to_file(hash)
+			hash = apified_hash hash
 			file = Tempfile.new(['coveralls-upload', 'json'])
 			file.write hash.to_json
 			File.new(file.path, 'rb')
+		end
+
+		def self.apified_hash hash
+			hash.merge(travis_job_id: ENV['TRAVIS_JOB_ID'])
 		end
 	
 	end
