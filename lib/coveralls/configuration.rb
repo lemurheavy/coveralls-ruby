20478d64583782fa9c6739e92acab53f4855f426
Nick Merwin <n@mer.io>
2016-02-24 14:22:20 -0800

    gitlab updated config

--- lib/coveralls/configuration.rb
+++ lib/coveralls/configuration.rb
@@ -89,12 +89,11 @@ module Coveralls
 
     def self.set_service_params_for_gitlab(config)
       config[:service_name]         = 'gitlab-ci'
-      config[:service_number]       = ENV['CI_BUILD_ID']
-      # config[:service_job_number]   = ENV['CI_BUILD_ID']
-      # config[:service_pull_request] = ENV['TDDIUM_PR_ID']
+      config[:service_number]       = ENV['CI_BUILD_REF'][0..5]
+      config[:service_job_number]   = ENV['CI_BUILD_NAME']
+      config[:service_job_id]       = ENV['CI_BUILD_ID']
       config[:service_branch]       = ENV['CI_BUILD_REF_NAME']
       config[:commit_sha]           = ENV['CI_BUILD_REF']
-      # config[:service_build_url]    = "https://ci.solanolabs.com/reports/#{ENV['TDDIUM_SESSION_ID']}"
     end
 
     def self.set_service_params_for_coveralls_local(config)
