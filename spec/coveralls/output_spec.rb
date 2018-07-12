157bf01974ea15f437b228debac7b3ab45ef201a
Vitor Oliveira <vbrazo@gmail.com>
2018-07-12 16:43:03 -0300

    Prefer expect than should and aggregate failures

--- spec/coveralls/output_spec.rb
+++ spec/coveralls/output_spec.rb
@@ -40,16 +40,21 @@ describe Coveralls::Output do
       Coveralls::Output.silent = true
       $stdout = @output
     end
-    it 'should not puts' do
-      Coveralls::Output.puts 'foo'
-      @output.rewind
-      @output.read.should == ''
+
+    it 'should not puts and print', :aggregate_failures do
+      aggregate_failures 'should not puts' do
+        Coveralls::Output.puts 'foo'
+        @output.rewind
+        expect(@output.read).to eq('')
+      end
+
+      aggregate_failures 'should not print' do
+        Coveralls::Output.print 'foo'
+        @output.rewind
+        expect(@output.read).to eq('')
+      end
     end
-    it 'should not print' do
-      Coveralls::Output.print 'foo'
-      @output.rewind
-      @output.read.should == ''
-    end
+
     after do
       $stdout = @original_stdout
     end
@@ -61,33 +66,32 @@ describe Coveralls::Output do
         require 'term/ansicolor'
         string = 'Hello'
         ansi_color_string = Term::ANSIColor.red(string)
-        Coveralls::Output.format(string, color: 'red').should eq(ansi_color_string)
+        expect(Coveralls::Output.format(string, color: 'red')).to eq(ansi_color_string)
       end
 
       aggregate_failures 'also accepts no color arguments' do
         unformatted_string = 'Hi Doggie!'
-        Coveralls::Output.format(unformatted_string).should eq(unformatted_string)
+        expect(Coveralls::Output.format(unformatted_string)).to eq(unformatted_string)
       end
 
       aggregate_failures 'rejects formats unrecognized by Term::ANSIColor' do
         string = 'Hi dog!'
-        Coveralls::Output.format(string, color: 'not_a_real_color').should eq(string)
+        expect(Coveralls::Output.format(string, color: 'not_a_real_color')).to eq(string)
       end
 
       aggregate_failures 'accepts more than 1 color argument' do
         string = 'Hi dog!'
         multi_formatted_string = Term::ANSIColor.red { Term::ANSIColor.underline(string) }
-        Coveralls::Output.format(string, color: 'red underline').should eq(multi_formatted_string)
+        expect(Coveralls::Output.format(string, color: 'red underline')).to eq(multi_formatted_string)
       end
     end
-    
+
     context 'no color' do
       before { Coveralls::Output.no_color = true }
 
       it 'does not add color to string' do
         unformatted_string = 'Hi Doggie!'
-        Coveralls::Output.format(unformatted_string, color: 'red')
-                         .should eq(unformatted_string)
+        expect(Coveralls::Output.format(unformatted_string, color: 'red')).to eq(unformatted_string)
       end
     end
   end
