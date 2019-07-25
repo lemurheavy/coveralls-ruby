87482e2084ff828fbd94057c8e261b5395fa4891
Ivan Sebastian <ivan.sebastian@bukalapak.com>
2019-07-25 11:09:11 +0700

    Change multipart content type delimiter

--- lib/coveralls/api.rb
+++ lib/coveralls/api.rb
@@ -90,7 +90,7 @@ module Coveralls
       boundary = rand(1_000_000).to_s
 
       request.body         = build_request_body(hash, boundary)
-      request.content_type = "multipart/form-data, boundary=#{boundary}"
+      request.content_type = "multipart/form-data; boundary=#{boundary}"
 
       request
     end
